6eef79e4aeab0b5ad6fa1d44adf084b8c89a08b0
Carlos Castillo <carloscastillo090916@gmail.com>
2019-01-29 16:23:07 -0500

    Agregando trabajo en clase tutoria

--- tutoria-clase15/principal.py
+++ tutoria-clase15/principal.py
@@ -12,17 +12,17 @@ m = MiArchivo() # objeto para leer el archivo
 lista = m.obtener_informacion() # Guardamos lo que retorna el metodo
 lista = [l.split(";") for l in lista] # Separamos por ';' el txt
 
-listado_edades = []
+listado_edades = [] # Lista que va a guardar las edades
 
 for x in lista:
-	p = Persona(x[0],x[1],x[2])
-	listado_edades.append(p.obtener_edad())
+	p = Persona(x[0],x[1],x[2]) # Creamos el objeto de tipo Persona y le enviamos los parametros
+	listado_edades.append(p.obtener_edad()) # Agregamos en la lista las edades
 
-merge_sort_result = merge_sort(listado_edades)  
+merge_sort_result = merge_sort(listado_edades)  # Guardamos en 'merge_sort_result' lo que retorna el metodo 'merge_sort' y le enviamos la lista de edades
 
-print(merge_sort_result)
+print(merge_sort_result) # Presentamos en pantalla
 
-m.cerrar_archivo()
+m.cerrar_archivo() # Cerramos el archivos
 
 
 
